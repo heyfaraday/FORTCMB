8952fc9903a17b11890b567df6975d8c9cf50611
Ivan Edigarev <heyfaraday@gmail.com>
2017-04-13 22:29:20 +0300

    map_coef final

--- lib/map_coef.f90
+++ lib/map_coef.f90
@@ -6,6 +6,9 @@ module map_coef
 
     subroutine a_lm_gasdev(p_lm_max, coef, seed, mean, std)
 
+      ! Healpix module
+      use healpix_types
+
       ! p_lm_max = maximum of l and m
       ! coef = array for writing
       ! seed = number for nr ran. num. generator
@@ -16,41 +19,39 @@ module map_coef
       use ran_state, only: ran_seed
 
       implicit none
-      integer, intent(in) :: p_lm_max
-      complex, dimension(0:p_lm_max, 0:p_lm_max), &
+      integer(kind=i8b), intent(in) :: p_lm_max
+      complex(kind=dpc), dimension(0:p_lm_max, 0:p_lm_max), &
                                   intent(out) :: coef
-      integer, intent(in) :: seed
-      double precision, intent(in) :: mean, std
+      integer(kind=i4b), intent(in) :: seed
+      real(kind=dp), intent(in) :: mean, std
 
-      integer :: m, l ! Var for iterating
-      real :: rand1, rand2 ! Vars for complex ran. num.
+      integer(kind=i8b) :: m, l ! Var for iterating
+      real(kind=sp) :: rand1_sp, rand2_sp ! Vars for complex ran. num.
 
       ! Initialization
-      coef = (0.d0, 0.d0)
+      coef = (0.0_dp, 0.0_dp)
 
       call ran_seed(seed)
 
       do l = 0, p_lm_max, 1
-        call gasdev(rand1)
-        coef(0, l) = cmplx(std * rand1 + mean, 0.d0 + mean)
+        call gasdev(rand1_sp)
+        call gasdev(rand2_sp)
+        coef(0, l) = cmplx(std * rand1_sp + mean, 0.0_dp + mean)
+        coef(l, l) = cmplx(std * rand2_sp + mean, 0.0_dp + mean)
       end do
 
       do m = 1, p_lm_max, 1
-        do l = m, p_lm_max, 1
-          call gasdev(rand1)
-          call gasdev(rand2)
-          coef(m, l) = cmplx(std * rand1 + mean, std * rand2 + mean)
+        do l = m + 1, p_lm_max, 1
+          call gasdev(rand1_sp)
+          call gasdev(rand2_sp)
+          coef(m, l) = cmplx(std * rand1_sp + mean, std * rand2_sp + mean)
         end do
       end do
 
-      do l = 0, p_lm_max, 1
-        coef(l, l) = coef(l, l) - dcmplx(0.d0, aimag(coef(l, l)))
-      end do
-
       ! 0- and 1- mode disabled
-      coef(0, 0) = (0.d0, 0.d0)
-      coef(0, 1) = (0.d0, 0.d0)
-      coef(1, 1) = (0.d0, 0.d0)
+      ! coef(0, 0) = (0.0_dp, 0.0_dp)
+      ! coef(0, 1) = (0.0_dp, 0.0_dp)
+      ! coef(1, 1) = (0.0_dp, 0.0_dp)
 
     end subroutine a_lm_gasdev
 
